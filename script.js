37bfb377754780c56976c5a902c6fe093be8738b
Tihami Rahman <138518399+TihamiDev@users.noreply.github.com>
2026-05-19 15:21:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 //your JS code here. If required.
-let name = document.selectId(fname);
-name.onBlur(toUpperCase);
+let name = document.getElementById("fname");
+name.onblur = function toUpper(params) {
+	name.value = name.value.toUpperCase();
+}
